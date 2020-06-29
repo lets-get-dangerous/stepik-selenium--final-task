f05fb31fbd1192b75b9bebb2a65a8e8e81e43dac
lets-get-dangerous <romcool777@gmail.com>
2020-06-29 17:31:55 +0800

    Created locators.py for pages. Added login_page object.

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+    p = type(browser.current_url)
+    assert False, p
